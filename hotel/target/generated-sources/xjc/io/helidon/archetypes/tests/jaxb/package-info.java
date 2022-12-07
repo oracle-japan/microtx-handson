d29becb1f82831e45debc244f89d79aba37acf9e
tniita <takuya2230@gmail.com>
2022-12-07 17:17:13 +0900

    prepare fix app

--- hotel/target/generated-sources/xjc/io/helidon/archetypes/tests/jaxb/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// このファイルは、JavaTM Architecture for XML Binding(JAXB) Reference Implementation、v2.3.0によって生成されました 
-// <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a>を参照してください 
-// ソース・スキーマの再コンパイル時にこのファイルの変更は失われます。 
-// 生成日: 2022.12.07 時間 02:36:05 PM JST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlns.jcp.org/xml/ns/persistence", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package io.helidon.archetypes.tests.jaxb;
