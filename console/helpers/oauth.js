bba5322a0be83f15dc69c801573835df1158f56c
tniita <takuya.niita@oracle.com>
2023-05-10 16:18:40 +0900

    fix the Vulnerability

--- console/helpers/oauth.js
+++ console/helpers/oauth.js
@@ -16,46 +16,35 @@ function getAT(username, password) {
         },
         body: 'grant_type=password&username=' + username + '&password=' + password + '&scope=' + encodeURIComponent(neededScopes.join(' '))
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to verify username and password.")
         }
         return response.json();
     }).then((data) => data.access_token
-    )};
+    )
+};
 exports.getAT = getAT;
 
 
-async function postTrip(accessToken, flightNumber, hotelName) {
+function postTrip(accessToken, flightNumber, hotelName) {
     const uri = new URL("?flightNumber=" + flightNumber + "&hotelName=" + hotelName, process.env.TRIP_SERVICE_URL);
-    const response = await fetch(uri, {
+    return fetch(uri, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json',
             'Authorization': 'Bearer ' + accessToken
         }
-    });
-    console.log(response.status)
-    if (response.status != 200) {
-        return reject("Error, Unable to create trip.");
-    }
-    if (response && response.status == 200) {
-        let oracleTmmTxToken;
-        if (response.headers && response.headers["oracle-tmm-tx-token"]) {
-            oracleTmmTxToken = response.headers["oracle-tmm-tx-token"];
-        }
-        logger.log("oracleTmmTxToken: \n" + oracleTmmTxToken);
-        return { 'oracleTmmTxToken': oracleTmmTxToken, 'body': body }
-    } else if (body) {
-        return { 'oracleTmmTxToken': undefined, 'body': body };
-    } else {
-        return reject("Error, Unable to create trip.");
-    }
+    }).then(response => {
+        return response.json();
+    }).then(responseData => {
+        return responseData;
+    })
 };
 exports.postTrip = postTrip;
 
 
 function getTrip(accessToken, bookingId) {
-    const uri = new URL("/" + encodeURIComponent(bookingId), process.env.TRIP_SERVICE_URL);
+    const uri = process.env.TRIP_SERVICE_URL + "/" + encodeURIComponent(bookingId);
     return fetch(uri, {
         method: 'GET',
         headers: {
@@ -64,69 +53,59 @@ function getTrip(accessToken, bookingId) {
             'Accept': 'application/json'
         }
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to get trip.")
         }
         return response.json();
-    }).then((data))};
+    }).then((data) => {
+        console.log(JSON.stringify(data));
+        return data;
+    });
+}
 exports.getTrip = getTrip;
 
 
-function confirmTrip(accessToken, bookingId, oracleTmmTxToken) {
-    const uri = new URL("/" + encodeURIComponent(bookingId), process.env.TRIP_SERVICE_URL);
-    let headers = {
-        'Content-type': 'application/json',
-        'Long-Running-Action': bookingId,
-        'Authorization': 'Bearer ' + accessToken,
-        'Accept': 'application/json'
-    };
-    if (oracleTmmTxToken) {
-        headers = {
+function confirmTrip(accessToken, bookingId) {
+    const uri = process.env.TRIP_SERVICE_URL + "/" + encodeURIComponent(bookingId);
+    return fetch(uri, {
+        method: 'PUT',
+        headers: {
             'Content-type': 'application/json',
             'Long-Running-Action': bookingId,
             'Authorization': 'Bearer ' + accessToken,
-            'Oracle-Tmm-Tx-Token': oracleTmmTxToken,
             'Accept': 'application/json'
-        };
-    }
-    return fetch(uri, {
-        method: 'PUT',
-        headers: headers
+        }
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to confirm trip.")
         }
-        return response.json();
-    }).then((data))};
+        return response;
+    }).then((data) => {
+        return data;
+    });
+}
 exports.confirmTrip = confirmTrip;
 
 
-function cancelTrip(accessToken, bookingId, oracleTmmTxToken) {
-    const uri = new URL("/" + encodeURIComponent(bookingId), process.env.TRIP_SERVICE_URL);
-    let headers = {
-        'Content-type': 'application/json',
-        'Long-Running-Action': bookingId,
-        'Authorization': 'Bearer ' + accessToken,
-        'Accept': 'application/json'
-    };
-    if (oracleTmmTxToken) {
-        headers = {
+function cancelTrip(accessToken, bookingId) {
+    const uri = process.env.TRIP_SERVICE_URL + "/" + encodeURIComponent(bookingId);
+    return fetch(uri, {
+        method: 'DELETE',
+        headers: {
             'Content-type': 'application/json',
             'Long-Running-Action': bookingId,
             'Authorization': 'Bearer ' + accessToken,
-            'Oracle-Tmm-Tx-Token': oracleTmmTxToken,
             'Accept': 'application/json'
-        };
-    }
-    return fetch(uri, {
-        method: 'DELETE',
-        headers: headers
+        }
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to cancel trip.")
         }
-        return response.json();
-    }).then((data))};
+        return response;
+    }).then((data) => {
+        return data;
+    });
+}
 exports.cancelTrip = cancelTrip;
 
 function getAllTrip(accessToken) {
@@ -139,11 +118,14 @@ function getAllTrip(accessToken) {
             'Accept': 'application/json'
         }
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to get trips.")
         }
         return response.json();
-    }).then((data))};
+    }).then((data) => {
+        return data;
+    });
+}
 exports.getAllTrip = getAllTrip;
 
 
@@ -157,9 +139,12 @@ function deleteAllTrip(accessToken) {
             'Accept': 'application/json'
         }
     }).then(response => {
-        if(response.status != 200){
+        if (response.status != 200) {
             throw new Error("Unable to delete trips.")
         }
-        return response.json();
-    }).then((data))};
+        return response;
+    }).then((data) => {
+        return data;
+    });
+}
 exports.deleteAllTrip = deleteAllTrip;
